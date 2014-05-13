1016c001fe7c08225327cec8d18bfa96a6e4cbd1
kevmelba <kevmelba@gmail.com>
2014-05-13 23:24:19 +0200

    Pointer on Function Operator

---
+++ Operator/and_simple.c
@@ -0,0 +1,17 @@
+/*
+** and_simple.c for and_simple in /home/gay_k/42sh
+** 
+** Made by Kevin Gay
+** Login   <gay_k@epitech.net>
+** 
+** Started on  Tue May 13 22:36:51 2014 Kevin Gay
+** Last update Tue May 13 22:49:09 2014 Kevin Gay
+*/
+
+#include "main.h"
+#include "tree.h"
+
+int	and_simple(t_node *tree, t_shell *sh)
+{
+  return (0);
+}
