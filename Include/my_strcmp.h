5f985608a6a428f6c2381e95c0a1fb2cc3218f5a
kevmelba <kevmelba@gmail.com>
2014-05-14 00:23:04 +0200

    new

--- Include/my_strcmp.h
+++
@@ -1,8 +0,0 @@
-#ifndef MY_STRCMP_H_
-# define MY_STRCMP_H_
-
-int	my_strcmp_(char *str1, char *str2);
-int	my_strcmp(char *str1, char *str2);
-int	my_strcmp_nbr(char *str1, char *str2, int o);
-
-#endif /* MY_STRCMP */
--- Include/my_strcpy.h
+++
@@ -1,6 +0,0 @@
-#ifndef MY_STRCPY_H_
-# define MY_STRCPY_H_
-
-char    *my_strcpy(char *dest, char *src);
-
-#endif /* MY_STRCPY */
