1e05b5ca67c3cab182eb2e6051db3ebfb355a6be
limone_m <limone_m@epitech.eu>
2014-05-19 13:40:24 +0200

    testk

--- Shell/shell.c
+++ Shell/shell.c
@@ -5,7 +5,7 @@
 ** Login   <limone_m@epitech.net>
 ** 
 ** Started on  Mon May  5 16:09:34 2014 Maxime Limone
-** Last update Fri May 16 14:32:07 2014 Maxime Limone
+** Last update Mon May 19 13:20:36 2014 Maxime Limone
 ** Last update Wed May 14 11:11:54 2014 Maxime Limone
 ** Last update Tue May 13 19:53:22 2014 Kevin Gay
 */
@@ -27,6 +27,7 @@
 #include "tree.h"
 #include "my_printf_error.h"
 #include "my_putstr.h"
+#include "epur_str.h"
 #include "ps1.h"
 
 int		read_buffer(t_shell *sh)
@@ -81,7 +82,7 @@ int		shell(t_shell *sh)
 	return (-1);
       if (read_buffer(sh) == -1)
 	return (-1);
-      if (parsing_main(sh->buffer, sh) == -1)
+      if (parsing_main(epur_str(sh->buffer), sh) == -1)
 	return (-1);
       free_shell(sh);
     }
